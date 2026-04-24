8785d75aeac2b166e125f715eb2ae57c2cf84441
johnDoe <77502134+rehon@users.noreply.github.com>
2026-04-24 11:28:07 +0500

    Terminal

---
+++ Terminal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using Common;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Terminal
+{
+    internal class Programm
+    {
+        public static async Task Main(string[] args)
+        {
+            List<News> lNews = new List<News>();
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(iPEndPoint);
+            if (socket.Connected)
+            {
+                lNews.Clear();
+                byte[] buffer = new byte[10485760];
+                string command = JsonConvert.SerializeObject("start");
+                buffer = Encoding.UTF8.GetBytes(command);
+                socket.Send(buffer);
+
+                int byteLength = socket.Receive(buffer);
+                string resive = JsonConvert.SerializeObject(byteLength);
+                lNews = JsonConvert.DeserializeObject<List<News>>(resive);
+                foreach(News New in lNews)
+                {
+                    Console.WriteLine(New.Name);
+                    Console.WriteLine(New.Date);
+                    Console.WriteLine(New.Src);
+                }
+            }
+        }
+    }
+}
