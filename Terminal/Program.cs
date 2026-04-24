3a540015c0c50bcb3a7769e183219c5ef3612707
johnDoe <77502134+rehon@users.noreply.github.com>
2026-04-24 12:04:19 +0500

    Dorabotca terminal

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -11,27 +11,34 @@ namespace Terminal
     {
         public static async Task Main(string[] args)
         {
-            List<News> lNews = new List<News>();
-            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(iPEndPoint);
-            if (socket.Connected)
+            try
             {
-                lNews.Clear();
-                byte[] buffer = new byte[10485760];
-                string command = JsonConvert.SerializeObject("start");
-                buffer = Encoding.UTF8.GetBytes(command);
-                socket.Send(buffer);
-
-                int byteLength = socket.Receive(buffer);
-                string resive = JsonConvert.SerializeObject(byteLength);
-                lNews = JsonConvert.DeserializeObject<List<News>>(resive);
-                foreach(News New in lNews)
+                List<News> lNews = new List<News>();
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5194);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(iPEndPoint);
+                if (socket.Connected)
                 {
-                    Console.WriteLine(New.Name);
-                    Console.WriteLine(New.Date);
-                    Console.WriteLine(New.Src);
+                    lNews.Clear();
+                    byte[] buffer = new byte[10485760];
+                    string command = JsonConvert.SerializeObject("start");
+                    buffer = Encoding.UTF8.GetBytes(command);
+                    socket.Send(buffer);
+
+                    int byteLength = socket.Receive(buffer);
+                    string resive = Encoding.UTF8.GetString(buffer, 0, byteLength);
+                    lNews = JsonConvert.DeserializeObject<List<News>>(resive);
+                    foreach (Common.News New in lNews)
+                    {
+                        Console.WriteLine(New.Name);
+                        Console.WriteLine(New.Date);
+                        Console.WriteLine(New.Src);
+                    }
                 }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
